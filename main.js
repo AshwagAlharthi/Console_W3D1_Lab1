e1d67b7b4424fec8354a826e0b9e3044a7a6dfd9
AshwagAlharthi <Ashwag.alharthi@hotmail.com>
2024-09-08 11:44:51 +0300

    add Question No4

--- main.js
+++ main.js
@@ -14,3 +14,15 @@ console.log(c_degree+"°C is " + f_degree + "°F");
 let hour = 12 ;
 let second = hour * 3600;
 console.log(hour + " hours = " + second + " Seconds");
+
+
+// Question No4
+/*
+ * REQUIREMENTS
+ * 1. Your code should have two variables -  `thingOne` and `thingTwo`
+ * 2. Your code should only have one `console.log` statement
+ * 3. Your code should print out the values of `thingOne` and `thingTwo` using concatenation
+ * 4. Your code should contain semicolons at the end of each line
+ */
+let thingOne = `Ashwag`, thingTwo = `Alharthi`;
+console.log(`${thingOne} ${thingTwo}`);
