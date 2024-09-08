aadf393e04cad8562c73c02f4b3599964cc218b0
AshwagAlharthi <Ashwag.alharthi@hotmail.com>
2024-09-08 11:44:10 +0300

    add Question No2

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ let width = 10;
 let height = 10;
 let result = width * height
 console.log("The area of the rectangle is: " , result);
+
+// Question No2
+let c_degree = 30;
+let f_degree =  c_degree * (9/5) + 32;
+console.log(c_degree+"°C is " + f_degree + "°F");
